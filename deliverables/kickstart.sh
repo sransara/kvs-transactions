026b9156c35e8b0bef6999dccbef8860b6cd9f0f
prashant-r <prashantr186@yahoo.com>
2016-04-15 07:02:37 -0400

    latest scripts

--- deliverables/kickstart.sh
+++ deliverables/kickstart.sh
@@ -1,12 +1,12 @@
 sh ./killall.sh
+sleep 5
 echo " Start Db Servers"
 count=1
 while [ $count -lt 6 ] && read line; do
 let count++
 stringarray=($line)
-echo $line
-ssh -T ${stringarray[0]} <<'ENDSSH0' &
+ssh -T ${stringarray[0]} <<ENDSSH0 &
 cd /u/antor/u7/ravi18/KVSTransactions/deliverables
-java -jar DbServer.jar $line	
+java -jar DbServer.jar $line
 ENDSSH0
 done < configs.txt
