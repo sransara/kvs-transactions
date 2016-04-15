d66d47dc1a2baf6f427b38867bc6abcdae8fc776
Prashant Ravi <prashant.ravi@gmail.com>
2016-04-15 05:50:07 -0400

    upload the kickstart script.

---
+++ deliverables/kickstart.sh
@@ -0,0 +1,9 @@
+sh ./killall.sh
+echo " Start Db Servers"
+while read line; do
+stringarray=($line)
+ssh -T ${stringarray[0]} <<'ENDSSH0' &
+cd /u/antor/u7/ravi18/KVSTransactions/deliverables
+java -jar DbServer.jar
+#ENDSSH0
+done < configs.txt
