69f105ccc61a89656a3dd2aa1e32194185b2538c
prashant-r <prashantr186@yahoo.com>
2016-04-15 05:24:47 -0400

    make sure kill script works

--- deliverables/killall.sh
+++ deliverables/killall.sh
@@ -1,7 +1,7 @@
-while read line; do   
+while read line; do
 	stringarray=($line) 
-	echo ' Killing Process on host with index # 1 '
+	echo ' Killing DbServer, Coordinator, Client on host ${stringarray[0]}'
 	ssh -T ${stringarray[0]} <<'ENDSSH0' &
-	jps -l | grep Process.jar | awk '{print $1}' | xargs kill -9
+	jps -l | egrep (DbServer.jar | Coordinator.jar | Client.jar) | awk '{print $1}' | xargs kill -9
 ENDSSH0
 done < configs.txt
