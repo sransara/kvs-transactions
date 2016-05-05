709d2f9dfcca075de65e9988ceae916cabdc7f16
Prashant Ravi <ravi18@purdue.edu>
2016-05-05 19:58:44 -0400

    merge shell start scripts.

--- deliverables/kickstart.sh
+++ deliverables/kickstart.sh
@@ -1,14 +1,43 @@
 sh ./killall.sh
-sleep 5
-echo " Start Db Servers"
+sleep 7
+echo " Start Coordinator and Db Servers"
+totalservers=70
 count=0
-while [ $count -lt 60 ] && read line; do
+joincount=0
+a=''
+while [ $count -lt $totalservers ] && read line; do
 let count++
 TEST=$(pwd)
-echo $TEST
+if [ $count -le 10 ];
+then
+JAR="Coordinator.jar"
+else
+JAR="DbServer.jar"
+fi
 stringarray=($line)
 ssh -T ${stringarray[0]} <<ENDSSH0 &
 cd $TEST
-java -jar DbServer.jar $line
+java -jar $JAR $line
 ENDSSH0
 done < configs.txt &
+sleep 5
+b=0
+echo "Joining servers"
+while [ $b -lt $totalservers ]; do
+let b++
+if [ $b -le 10 ];
+then
+: nop
+else
+let joincount++
+a+=' '$b
+fi
+if [[ $joincount == 5 ]]; then
+java -jar CoordinatorClient.jar JOIN $a
+echo $a
+a=''
+let joincount=0
+else
+: nop
+fi
+done
